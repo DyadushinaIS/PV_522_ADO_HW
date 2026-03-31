900e02200a8dbb32dc5c5602939b0fc188200f0b
User <User@DESKTOP-QHG18FL>
2026-03-31 21:43:57 +0300

    System.Configuration DONE

--- PV_522_ADO/Program.cs
+++ PV_522_ADO/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Data.SqlClient;
+using System.Configuration;
 
 //ADO.NET
 //ADO - ActiveX Data Objects
@@ -16,10 +17,10 @@ namespace PV_522_ADO
 {
 	class Program
 	{
-		
 		static void Main(string[] args)
 		{
-			string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_522;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			//string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_522;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			string connection_string = ConfigurationManager.ConnectionStrings["Movies"].ConnectionString;
 			Connector connector = new Connector(connection_string);
 
 			connector.Select("SELECT * FROM Directors");
--- ReadMe.txt
+++ ReadMe.txt
@@ -2,6 +2,10 @@
 https://www.youtube.com/playlist?list=PLeqyOOqxeiIN0ubAtUHCDYyJY_ob19ONJ
 
 TODO:
+1. Создать ветку 'HW_2';
+2. В класс 'Connector' добавить метод Insert(), который позволяет выполнять вставку данных в таблицу Базы;
+
+DONE:
 1. Создать репозиторий и залить его на GitHub;
 2. Сделать ветку 'HW_1';
 3. В ветке 'HW_1' все методы для работы с Базой: Select() и Scalar() вынести в отдельный класс;
